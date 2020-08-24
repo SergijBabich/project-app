7620f7af47d561836161511fc59be4407647757e
SerijBabich <moran.a@list.ru>
2020-08-24 14:12:44 +0300

    Updates

---
+++ precache-manifest.663bdd473bf1af4fcea1f73a5e23916b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230dff0bbd034cbf1651334c219f976d",
+    "url": "/project-app/index.html"
+  },
+  {
+    "revision": "6e83295328fc27889dba",
+    "url": "/project-app/static/css/main.5b8ffb3a.chunk.css"
+  },
+  {
+    "revision": "782068e9eff1c4f651b8",
+    "url": "/project-app/static/js/2.110f9860.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-app/static/js/2.110f9860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e83295328fc27889dba",
+    "url": "/project-app/static/js/main.d23ca191.chunk.js"
+  },
+  {
+    "revision": "d8f4d88f237a42534e42",
+    "url": "/project-app/static/js/runtime-main.7b0b9174.js"
+  }
+]);
